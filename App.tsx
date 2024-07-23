24b319929fcbe60d7ad47c7b1a66ccf1a15f77a0
HydronSid <suvisiddhant6101999@gmail.com>
2024-07-23 18:30:17 +0530

    added get random image

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {HomeScreen} from './lib/Screens/HomeScreen/home-screen';
+import {MyTabs} from './lib/screens/tab-screens/tab-screen';
+import {Provider as PaperProvider} from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Tabs"
+            component={MyTabs}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
